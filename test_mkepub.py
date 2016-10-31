e0de58f696cae22aa73163f825546b07e005b639
anqxyr <anqxyr@gmail.com>
2016-10-31 22:29:16 +0300

    modify epubcheck installation

--- test_mkepub.py
+++ test_mkepub.py
@@ -2,45 +2,47 @@
 """mkepub Test Suite."""
 
 import mkepub
+import pathlib
 import subprocess
 
 ###############################################################################
 
+if not pathlib.Path('test_output/').exists():
+    pathlib.Path('test_output/').mkdir()
+
+
+def save_and_check(book):
+    name = 'test_output/{}.epub'.format(book.title.lower().replace(' ', '_'))
+    book.save(name)
+    subprocess.check_output(['epubcheck', name])
+
 
 def test_book_simple():
-    book = mkepub.Book(title='Simple Test')
+    book = mkepub.Book(title='Simple')
     with open('cover.jpg', 'rb') as coverfile:
         book.set_cover(coverfile.read())
     book.add_page('Page 1', 'Content of the first page.')
     book.add_page('Page 2', 'Content of the second page.')
-    book.save('simple.epub')
-
-    subprocess.check_output(['epubcheck', 'simple.epub'])
+    save_and_check(book)
 
 
 def test_book_no_cover():
-    book = mkepub.Book(title='No Cover Test')
+    book = mkepub.Book(title='No Cover')
     book.add_page('Page Only', 'This is all the text.')
-    book.save('nocover.epub')
-
-    subprocess.check_output(['epubcheck', 'nocover.epub'])
+    save_and_check(book)
 
 
 def test_book_one_author():
-    book = mkepub.Book(title='Whatever', author='This Guy')
+    book = mkepub.Book(title='One Author', author='This Guy')
     book.add_page('Page 1', 'text text. text.')
-    book.save('one_author.epub')
-
-    subprocess.check_output(['epubcheck', 'one_author.epub'])
+    save_and_check(book)
 
 
 def test_book_multiple_authors():
     book = mkepub.Book(
-        title='Still Whatever', authors=['This Guy', 'Some Other Guy'])
+        title='Multiple Authors', authors=['This Guy', 'Some Other Guy'])
     book.add_page('Page 1', 'text text. text.')
-    book.save('multiple_authors.epub')
-
-    subprocess.check_output(['epubcheck', 'multiple_authors.epub'])
+    save_and_check(book)
 
 
 def test_book_nested():
@@ -49,9 +51,4 @@ def test_book_nested():
     book.add_page('First Child', 'ヽ(。_°)ノ', parent)
     second_child = book.add_page('Second Child', 'blop', parent)
     book.add_page('Grandkid', 'ooOOoo', second_child)
-    book.save('nested.epub')
-
-    subprocess.check_output(['epubcheck', 'nested.epub'])
-
-
-
+    save_and_check(book)
