99fe65efc8326beee1a36af21c801a32707af3a6
anqxyr <anqxyr@gmail.com>
2016-10-31 13:07:45 +0300

    major overhaul; move to epub3 and jinja2

---
+++ mkepub.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+
+"""
+Create Epub files.
+
+This code was designed to provide a very simple and straight-forward API for
+creating epub files, by sacrificing most of the versatility of the format.
+
+Example usage:
+
+>>> book = Book(title='Example Book', author='John Doe')
+>>> with open('cover.png', 'br') as file:
+>>>     book.set_cover(file.read())
+>>> with open('style.css') as file:
+>>>     book.set_stylesheet(file.read())
+>>> book.add_page(title='First Page', content='some text')
+>>> chapter = book.add_page(title='First Chapter', content='more text')
+>>> book.add_page(
+>>>     title='Sub-Page 1',
+>>>     content='first subpage of the chapter',
+>>>     parent=chapter)
+>>> with open('image.jpg', 'br') as file:
+>>>     book.add_image('image.jpg', file.read())
+>>> book.save('example.epub')
+
+"""
+
+###############################################################################
+# Module Imports
+###############################################################################
+
+import collections
+import datetime
+import imghdr
+import itertools
+import jinja2
+import pathlib
+import tempfile
+import uuid
+import zipfile
+
+
+###############################################################################
+
+env = jinja2.Environment(loader=jinja2.PackageLoader('mkepub'))
+env.filters['mediatype'] = lambda x: 'image/' + x.split('.')[-1]
+
+###############################################################################
+
+Page = collections.namedtuple('Page', 'page_id title children')
+Image = collections.namedtuple('Image', 'image_id name')
+
+
+class Book:
+    """EPUB book."""
+
+    def __init__(self, title, **metadata):
+        """"Create new book."""
+        self.title = title
+        self.metadata = metadata
+
+        self.tempdir = tempfile.TemporaryDirectory()
+        self.root = []
+        self.images = []
+        self._page_id = map('{:04}'.format, itertools.count(1))
+        self._image_id = map('{:03}'.format, itertools.count(1))
+
+        self.path = pathlib.Path(self.tempdir.name).resolve()
+        (self.path / 'EPUB/images').mkdir(parents=True)
+        (self.path / 'EPUB/css').mkdir(parents=True)
+        (self.path / 'META-INF').mkdir()
+
+        self.set_stylesheet('')
+        self._cover = None
+
+    ###########################################################################
+    # Public Methods
+    ###########################################################################
+
+    def add_page(self, title, content, parent=None):
+        """
+        Add a new page.
+
+        The page will be added as a subpage of the parent. If no parent is
+        provided, the page will be added to the root of the book.
+        """
+        page = Page(next(self._page_id), title, [])
+        self.root.append(page) if not parent else parent.children.append(page)
+        self._write_page(page, content)
+        return page
+
+    def add_image(self, name, data):
+        """Add image file."""
+        self.images.append(Image(next(self._image_id), name))
+        with open(str(self.path / 'images' / name), 'wb') as file:
+            file.write(data)
+
+    def set_cover(self, data):
+        """Set the cover image to the given png data."""
+        self._cover = 'cover.' + imghdr.what(None, h=data)
+        with open(str(self.path / 'EPUB' / self._cover), 'wb') as file:
+            file.write(data)
+
+    def set_stylesheet(self, data):
+        """Set the stylesheet to the given css data."""
+        with open(str(self.path / 'EPUB/css/stylesheet.css'), 'w') as file:
+            file.write(data)
+
+    def save(self, filename):
+        """Save book to a file."""
+        self._write_spine()
+        self._write_container()
+        self._write_toc()
+        with open(str(self.path / 'mimetype'), 'w') as file:
+            file.write('application/epub+zip')
+        with zipfile.ZipFile(filename, 'w') as archive:
+            archive.write(
+                str(self.path / 'mimetype'), 'mimetype',
+                compress_type=zipfile.ZIP_STORED)
+            for file in self.path.rglob('*.*'):
+                archive.write(
+                    str(file), str(file.relative_to(self.path)),
+                    compress_type=zipfile.ZIP_DEFLATED)
+
+    ###########################################################################
+    # Private Methods
+    ###########################################################################
+
+    def _write(self, template, path, **data):
+        with open(str(self.path / path), 'w') as file:
+            file.write(env.get_template(template).render(**data))
+
+    def _write_page(self, page, content):
+        """Write the contents of the page into an html file."""
+        self._write(
+            'page.xhtml', 'EPUB/page{}.xhtml'.format(page.page_id),
+            title=page.title, body=content)
+
+    def _write_spine(self):
+        self._write(
+            'package.opf', 'EPUB/package.opf',
+            time=datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ'),
+            pages=list(self._flatten(self.root)), images=self.images,
+            uuid=uuid.uuid4(), cover=self._cover,
+            **self.metadata)
+
+    def _write_container(self):
+        self._write('container.xml', 'META-INF/container.xml')
+
+    def _write_toc(self):
+        self._write(
+            'toc.xhtml', 'EPUB/toc.xhtml', pages=self.root, title=self.title)
+
+    def _flatten(self, tree):
+        for item in tree:
+            yield item
+            yield from self._flatten(item.children)
--- test_mkepub.py
+++ test_mkepub.py
@@ -6,34 +6,16 @@ import pathlib
 import mkepub
 
 
-def test_empty_book():
-    """
-    Test creation of an empty book.
-
-    This test is only to make sure that no exceptions are raised during
-    the creation of the book, and that a file exists afterward. It does not
-    check the contents of the file.
-    """
-    file = pathlib.Path('test.epub')
+def test_book_simple():
+    file = pathlib.Path('simple.epub')
     if file.exists():
         file.unlink()
-    book = mkepub.Book()
-    book.add_page('-', '-')
+    book = mkepub.Book(title='Simple Test')
+    with open('cover.jpg', 'rb') as coverfile:
+        book.set_cover(coverfile.read())
+    book.add_page('First page', 'Content of the first page.')
+    book.add_page('Second page', 'Content of the second page.')
+    book.add_page('Third page', 'Content of the third page.')
     book.save(str(file))
     assert file.exists()
 
-
-def test_page_add():
-    """Test addition of a page to the book."""
-    book = mkepub.Book()
-    page = book.add_page('-', '-')
-    path = book.path / 'pages' / (page.uid + '.xhtml')
-    assert path.exists()
-
-
-def test_page_add_child():
-    """Test child pages."""
-    book = mkepub.Book()
-    parent = book.add_page('-', '-')
-    child = book.add_page('-', '-', parent)
-    assert child in parent.children
